368fed4848d9b6f1d35eb5be9830212da5d25e80
Trista Runions <t.runions19@gmail.com>
2021-08-15 13:03:25 -0700

    fixed tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const Tabs = ({topics}) => {
+const Tabs = (topics) => {
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -16,19 +16,34 @@ const Tabs = ({topics}) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-  const topicsDIV = document.createElement('div') 
-  topicsDIV.classList.add('topics')               
+  const topicDiv = document.createElement('div') 
+  const tab1 = document.createElement('div');
+  const tab2 = document.createElement('div');
+  const tab3 = document.createElement('div');
+  const tab4 = document.createElement('div');
+  const tab5 = document.createElement('div');
 
-  topics.forEach((i) => {                         
-    const tabs = document.createElement('div')  
-    tabs.classList.add('tab')                 
-    tabs.textContent = i;                     
-    topicsDIV.appendChild(tabs) 
-  })
-                
+  topicDiv.appendChild(tab1);
+  topicDiv.appendChild(tab2);
+  topicDiv.appendChild(tab3);
+  topicDiv.appendChild(tab4);
+  topicDiv.appendChild(tab5);
+
+  topicDiv.classList.add('topics');
+  tab1.classList.add('tab');
+  tab2.classList.add('tab');
+  tab3.classList.add('tab');
+  tab4.classList.add('tab');
+  tab5.classList.add('tab');
+
+  tab1.textContent = topics[0];
+  tab2.textContent = topics[1];
+  tab3.textContent = topics[2];
+  tab4.textContent = topics[3];
+  tab5.textContent = topics[4];
                                                   
                                                  
-  return topicsDIV;                               
+  return topicDiv;                               
 }
 
 
@@ -44,7 +59,7 @@ const tabsAppender = (selector) => {
 
   axios.get(`http://localhost:5000/api/topics`)
   .then(res => {
-    const topicArr = res.data
+    const topicArr = res.data.topics
     const newTab = Tabs(topicArr)
     const tabAnchor = document.querySelector(selector)
     tabAnchor.appendChild(newTab)
