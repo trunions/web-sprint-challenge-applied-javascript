ffaf50dde1be269bd452a9c957435c209ae4de71
Trista Runions <t.runions19@gmail.com>
2021-08-15 12:31:12 -0700

    finished card

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,6 @@
+import axios from "axios"
+
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +20,33 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const card = document.createElement('div')
+  const headline = document.createElement('div')
+  const author = document.createElement('div')
+  const imgContainer = document.createElement('div')
+  const img = document.createElement('img')
+  const authorName = document.createElement('span')
+
+  card.appendChild(headline)
+  card.appendChild(author)
+  author.appendChild(imgContainer)
+  author.appendChild(authorName)
+  imgContainer.appendChild(img)
+
+  card.classList.add('card')
+  headline.classList.add('headline')
+  author.classList.add('author')
+  imgContainer.classList.add('img-container')
+
+  headline.textContent = article.headline
+  img.setAttribute('src', article.authorPhoto)
+  authorName.textContent = article.authorName
+
+  card.addEventListener('click', e => console.log(e))
+
+  return card
+ 
+
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +58,19 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  const articleAnchor = document.querySelector(selector)
+
+  axios.get(`http://localhost:5000/api/articles`)
+  .then(res => {
+    const articles = res.data.articles
+    for(const article in articles){
+      articles[article].forEach(i => {
+        articleAnchor.appendChild(Card(i))
+      })
+    }
+
+  })
+  .catch(err => console.log(err))
 }
 
 export { Card, cardAppender }
